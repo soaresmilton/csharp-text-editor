6dcc2e2f51e2ffe4e7c0b2d678c73b19392bbf92
soaresmilton <eng.milton.soares@gmail.com>
2022-11-03 22:13:14 -0300

    Finalizing app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TextEditor // Note: actual namespace depends on the project name.
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("O que você deseja fazer?");
+      Console.WriteLine("1 - Abrir arquivo");
+      Console.WriteLine("2 - Criar novo arquivo");
+      Console.WriteLine("0 - Sair");
+
+      short option = short.Parse(Console.ReadLine());
+
+      switch (option)
+      {
+        case 0: System.Environment.Exit(0); break;
+        case 1: AbrirArquivo(); break;
+        case 2: CriarNovoArquivo(); break;
+        default: Menu(); break;
+
+      }
+
+
+    }
+
+    static void AbrirArquivo()
+    {
+      Console.Clear();
+      Console.WriteLine("Qual caminho do arquivo?");
+
+      string path = Console.ReadLine();
+
+      using (var file = new StreamReader(path))
+      {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+      }
+
+      Console.WriteLine("");
+      Console.ReadLine();
+      Menu();
+    }
+
+    static void CriarNovoArquivo()
+    {
+      Console.Clear();
+      Console.WriteLine("Digite seu texto abaixo (ESC para sair).");
+      Console.WriteLine("-------------------------");
+      string text = "";
+
+      do
+      {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+      }
+      while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+      Console.Write(text);
+      Salvar(text);
+    }
+
+    static void Salvar(string text)
+    {
+      Console.Clear();
+      Console.WriteLine("Qual caminho para salvar o arquivo?");
+      var path = Console.ReadLine();
+
+      using (var file = new StreamWriter(path))
+      {
+        file.Write(text);
+      }
+
+      Console.WriteLine($"Arquivo {path} salvo com sucesso.");
+      Console.ReadLine();
+      Menu();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+### Text Editor App
+
+This application is a simple Text file editor.
+
+Through the CLI, we can write texts into .txt files, that will be saved on a path of our choice.
+
+This applications was built with C#.
+
+### How it works
+
+- Run `dotnet run` in your terminal;
+- Pick a option from the menu:
+  0 - Exit
+  1 - Open file
+  2 - Create file
+
+  IF 1, you will be able to read the existent content of a txt file, from a specified path. Then the application will print the file content in the terminal.
+
+  IF 2, you will be able to write a text in a .txt file. Which path will be set via CLI.
+
+  IF 0, you will finish the app.
+
+  <div align ="center">
+    <img src="./text-editor.gif" width="700">
+  </div>
